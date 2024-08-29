4cd6128d49f9b47817a618bd0b63cf1a0e930fee
Paul <azaladeus@gmail.com>
2024-08-29 14:24:36 +0700

    Add comments

--- javaScript/stringEndsWith/index.js
+++ javaScript/stringEndsWith/index.js
@@ -1,3 +1,5 @@
+//Решил ее так (str.slice(-ending.length) === ending || ending == '') ? true : false; Оказывается все можно было сделать проще.
+//Функция endsWith и проверяет конец строки.
 function solution(str, ending){
     return str.endsWith(ending);
 }
